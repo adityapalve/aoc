55c2b91f193ae4e4c5e3b588759faf4d038e9338
adi <aditya4palve@gmail.com>
2024-12-01 10:07:09 +0000

    day 1

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,2 +1,26 @@
 with open('input.txt') as f:
-    data = f.read()
+    data = f.readlines()
+
+
+def foo():
+    left, right = [], []
+    for line in data:
+        l, r = line.strip().split()
+        left.append(int(l))
+        right.append(int(r))
+    left.sort()
+    right.sort()
+    s = 0
+    # for l, r in zip(left, right):
+    #     s = s + abs(l-r)
+    # return s
+    score = 0
+    for l in left:
+        freq = 0
+        for r in right:
+            if l == r:
+                freq += 1
+        score = score + l*freq
+    return score
+
+print(foo())
